e62e5ee010a049a8c772d6cc8ec878a8befacc8a
Sheena <sheena.salmon@gmail.com>
2020-09-15 19:48:58 -0400

    All the checkboxes and toal

--- js/script.js
+++ js/script.js
@@ -5,6 +5,12 @@ document.addEventListener("DOMContentLoaded",(e) =>
     const jspunsColor=document.getElementById("jspuns-color");
     const jsheartColor=document.getElementById("jsheart-color");
     const themeColor=document.getElementById("theme-color");
+    const activityCheckbox=document.querySelector(".activities");
+    const totalField=document.createElement("p");
+    activityCheckbox.insertAdjacentElement("beforeend",totalField);
+    totalField.innerText="Total Price: ";
+    let total= 0;
+
     console.log(themeColor);
     jsheartColor.style.display="none";
     jspunsColor.style.display="none";
@@ -31,7 +37,9 @@ document.addEventListener("DOMContentLoaded",(e) =>
     
 
     designMenu.addEventListener("change",(e)=>
+    
     {
+        
         if(e.target.value=="js puns")
             {
                 themeColor.style.display="none";
@@ -47,13 +55,56 @@ document.addEventListener("DOMContentLoaded",(e) =>
             jsheartColor.style.display="inline";
         }
     })
+activityCheckbox.addEventListener("change",(e) =>
+{
+    if(e.target.type == 'checkbox')
+    {
+     
+       let tagInput= activityCheckbox.getElementsByTagName("input");
+       if (e.target.checked==false)
+       {
+        let dateTime=e.target.getAttribute("data-day-and-time");
+        total -=parseFloat(e.target.getAttribute("data-cost"));
+           for(let i=0; i< tagInput.length ;i++)
+           {
+            if(dateTime==tagInput[i].getAttribute("data-day-and-time"))
+            {
+                tagInput[i].disabled=false;
+
+            }
+           }
+           
+       }
+       
+       else{
+        let dateTime=e.target.getAttribute("data-day-and-time");
+        console.log(dateTime);
+        total +=parseFloat(e.target.getAttribute("data-cost"));
+
+        //console.log(tagInput);
+        for(let i=0; i<tagInput.length; i++)
+        {
+           // console.log(tagInput[i].getAttribute("data-day-and-time"));
+           if(dateTime==tagInput[i].getAttribute("data-day-and-time"))
+           {
+               tagInput[i].disabled=true;
+           }
+
+
+        }
+        e.target.disabled=false;
+
+       }
+      
+       totalField.innerText=`Total Price: $${total}`;
+    }
+})
 
 
 
 
 
-
-
+ 
 
 
 
